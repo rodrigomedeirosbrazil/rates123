49bea77ea4e25297333c9fbb6967e5d9876429b1
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-01-27 19:14:17 -0300

    chore: add native filament button and set space from form

--- resources/views/livewire/calculator-form.blade.php
+++ resources/views/livewire/calculator-form.blade.php
@@ -1,15 +1,11 @@
-<div class="mt-16">
-    <h2 class="text-2xl font-bold tracing-tight mb-4">Calculator</h2>
+<div>
+    <form wire:submit="calc" class="space-y-6">
+        {{ $this->form }}
 
-    <div class="p-6 rounded-md shadow-sm">
-        <form wire:submit="calc">
-            {{ $this->form }}
-
-            <button type="submit" class="font-bold rounded-md">
-                Calc
-            </button>
-        </form>
-    </div>
+        <x-filament::button type="submit" class="mt-4">
+            Calc
+        </x-filament::button>
+    </form>
 
     <x-filament-actions::modals />
 </div>
