534c0217c644399a2f4c157117375d5dac8ae87c
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-02-21 20:26:01 -0300

    chore: wip

--- resources/views/livewire/calculator-form.blade.php
+++ resources/views/livewire/calculator-form.blade.php
@@ -3,9 +3,32 @@
         {{ $this->form }}
 
         <x-filament::button type="submit" class="mt-4">
-            Calc
+            {{ __('Calculate') }}
         </x-filament::button>
     </form>
+
+    <div class="min-w-full align-middle m-4">
+        <table class="min-w-full border divide-y divide-gray-200">
+            <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                @forelse($receiptItems as $receiptItem)
+                    <tr class="bg-white" wire:key="{{ $receiptItem?->name }}">
+                        <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                            {{ __($receiptItem?->name) }}
+                        </td>
+                        <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                            {{ $receiptItem?->amount }}
+                        </td>
+                    </tr>
+                @empty
+                    <tr class="bg-white">
+                        <td colspan="2" class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                            No items found.
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 
     <x-filament-actions::modals />
 </div>
