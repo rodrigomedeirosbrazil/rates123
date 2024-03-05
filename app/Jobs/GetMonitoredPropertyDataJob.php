c406730694e7f4cf0cbbfb0dc8eaa4bc905a63e5
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-05 20:42:04 -0300

    chore: add monitored sync to job

--- app/Jobs/GetMonitoredPropertyDataJob.php
+++ app/Jobs/GetMonitoredPropertyDataJob.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Managers\ScrapManager;
 use App\Models\MonitoredData;
 use App\Models\MonitoredProperty;
+use App\Models\MonitoredSync;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -22,21 +23,37 @@ class GetMonitoredPropertyDataJob implements ShouldQueue
 
     public function handle(ScrapManager $scrapManager): void
     {
+        $sync = MonitoredSync::create([
+            'monitored_property_id' => $this->monitoredPropertyId,
+            'successful' => false,
+            'prices_count' => 0,
+            'started_at' => now(),
+            'finished_at' => null,
+        ]);
+
         $property = MonitoredProperty::findOrFail($this->monitoredPropertyId);
 
-        $scrapManager->getPrices($property->url, $property->capture_months_number)
-            ->each(
-                fn ($price) => MonitoredData::create([
-                    'monitored_property_id' => $property->id,
-                    'price' => human_readable_size_to_int(
-                        data_get($price, 'avgPriceFormatted') ?? '0'
-                    ),
-                    'checkin' => data_get($price, 'checkin'),
-                    'available' => data_get($price, 'available') ?? false,
-                    'extra' => [
-                        'minLengthOfStay' => data_get($price, 'minLengthOfStay'),
-                    ],
-                ])
-            );
+        $prices = $scrapManager->getPrices($property->url, $property->capture_months_number);
+
+        $sync->prices_count = count($prices);
+        $sync->save();
+
+        $prices->each(
+            fn ($price) => MonitoredData::create([
+                'monitored_property_id' => $property->id,
+                'price' => human_readable_size_to_int(
+                    data_get($price, 'avgPriceFormatted') ?? '0'
+                ),
+                'checkin' => data_get($price, 'checkin'),
+                'available' => data_get($price, 'available') ?? false,
+                'extra' => [
+                    'minLengthOfStay' => data_get($price, 'minLengthOfStay'),
+                ],
+            ])
+        );
+
+        $sync->successful = true;
+        $sync->finished_at = now();
+        $sync->save();
     }
 }
