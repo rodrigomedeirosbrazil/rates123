1f3ac70834fe5acce685319b0dbc98e0a9d2ff28
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-05 12:52:31 -0300

    chore: adjust queue

--- config/horizon.php
+++ config/horizon.php
@@ -56,7 +56,7 @@ return [
 
     'prefix' => env(
         'HORIZON_PREFIX',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_horizon:'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_horizon:'
     ),
 
     /*
@@ -201,6 +201,7 @@ return [
                 'maxProcesses' => 10,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
+                'timeout' => 110,
             ],
         ],
 
