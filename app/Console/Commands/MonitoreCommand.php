15585492dec89213c75dcaef33481367efc42760
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-06 19:12:23 -0300

    chore: add check $isSyncedSuccesfulToday

--- app/Console/Commands/MonitoreCommand.php
+++ app/Console/Commands/MonitoreCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Jobs\GetMonitoredPropertyDataJob;
 use App\Models\MonitoredProperty;
+use App\Models\MonitoredSync;
 use Illuminate\Console\Command;
 
 class MonitoreCommand extends Command
@@ -29,10 +30,22 @@ class MonitoreCommand extends Command
     {
         MonitoredProperty::all()
             ->each(
-                fn (MonitoredProperty $property, int $index) => dispatch(
-                    new GetMonitoredPropertyDataJob($property->id)
-                )
-                    ->delay(now()->addMinutes($index * 1))
+                function (MonitoredProperty $property, int $index) {
+                    $isSyncedSuccesfulToday = MonitoredSync::query()
+                        ->whereMonitoredPropertyId($property->id)
+                        ->whereDate('started_at', now())
+                        ->whereSuccessful(true)
+                        ->exists();
+
+                    if ($isSyncedSuccesfulToday) {
+                        return;
+                    }
+
+                    dispatch(
+                        new GetMonitoredPropertyDataJob($property->id)
+                    )
+                        ->delay(now()->addMinutes($index * 1));
+                }
             );
     }
 }
