beb7dd21af036393ca175389ce842c30b5e9b386
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-09 10:11:53 -0300

    chore: add check prices to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,6 +10,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:monitore')->dailyAt('12:00');
+        $schedule->command('app:check-prices')->dailyAt('15:00');
     }
 
     protected function commands(): void
