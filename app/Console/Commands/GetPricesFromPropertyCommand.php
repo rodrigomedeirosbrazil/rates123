8f6caf31dc2b4a7608464189eebfd46d751df911
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-13 20:58:10 -0300

    chore add GetPricesFromPropertyCommand

---
+++ app/Console/Commands/GetPricesFromPropertyCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Managers\ScrapManager;
+use App\Models\MonitoredProperty;
+use Illuminate\Console\Command;
+
+class GetPricesFromPropertyCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'property:get-prices
+                            {propertyId : The property ID}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Scrap data from a property';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $propertyId = $this->argument('propertyId');
+        $property = MonitoredProperty::find($propertyId);
+
+        if (! $property) {
+            $this->error("Couldn't find a property with ID {$propertyId}");
+
+            return 1;
+        }
+
+        $propertyDTO = $property->toPropertyDTO();
+
+        $scrapManager = new ScrapManager();
+
+        $startTimestamp = now();
+        $prices = $propertyDTO->platformSlug === 'booking'
+                ? $scrapManager->getPrices($propertyDTO, now()->addDay(), config('platforms.booking.scrap_days'))
+                : $scrapManager->getPrices($propertyDTO, now()->addDay(), config('platforms.airbnb.scrap_days'));
+
+        $this->table(['Check In', 'Price', 'Available'], $prices->map(fn ($price) => [$price->checkin->toDateString(), $price->price, $price->available])->toArray());
+
+        $this->line('Started at: ' . $startTimestamp->toDateTimeString());
+        $this->line('Finished at: ' . now()->toDateTimeString());
+        $this->line('Elapsed seconds: ' . $startTimestamp->diffInSeconds(now()));
+    }
+}
