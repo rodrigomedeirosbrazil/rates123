4309c4f98a62398f3b7699b2eedb8e7b362483e7
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-19 20:53:43 -0300

    chore: add CapturePricesFromPropertyCommand

---
+++ app/Console/Commands/CapturePricesFromPropertyCommand.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\CheckPropertyPricesJob;
+use App\Managers\ScrapManager;
+use App\Models\MonitoredData;
+use App\Models\MonitoredProperty;
+use App\Models\MonitoredSync;
+use Illuminate\Console\Command;
+
+use function Laravel\Prompts\confirm;
+
+class CapturePricesFromPropertyCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'property:capture-prices
+                            {propertyId : The property ID}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Scrap and store data from a property';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $propertyId = $this->argument('propertyId');
+        $property = MonitoredProperty::find($propertyId);
+
+        if (! $property) {
+            $this->error("Couldn't find a property with ID {$propertyId}");
+
+            return 1;
+        }
+
+        $isSyncedSuccesfulToday = MonitoredSync::query()
+            ->whereMonitoredPropertyId($propertyId)
+            ->whereDate('started_at', now())
+            ->whereSuccessful(true)
+            ->exists();
+
+        if (
+            $isSyncedSuccesfulToday
+            && ! confirm('This property has already been synced sucessful today. Do you want to continue?', false)
+        ) {
+            return 0;
+        }
+
+        $propertyDTO = $property->toPropertyDTO();
+
+        $scrapManager = new ScrapManager();
+
+        $startTimestamp = now();
+
+        $sync = MonitoredSync::create([
+            'monitored_property_id' => $propertyId,
+            'successful' => false,
+            'prices_count' => 0,
+            'started_at' => now(),
+            'finished_at' => null,
+        ]);
+
+        try {
+            $prices = $propertyDTO->platformSlug === 'booking'
+                ? $scrapManager->getPrices($propertyDTO, now()->addDay(), config('platforms.booking.scrap_days'))
+                : $scrapManager->getPrices($propertyDTO, now()->addDay(), config('platforms.airbnb.scrap_days'));
+        } catch (\Exception $e) {
+            $sync->successful = false;
+            $sync->finished_at = now();
+            $sync->save();
+
+            $this->error($e->getMessage());
+
+            return 1;
+        }
+
+        $prices->each(
+            fn ($price) => MonitoredData::create([
+                'monitored_property_id' => $propertyId,
+                'price' => $price->price,
+                'checkin' => $price->checkin,
+                'available' => $price->available,
+                'extra' => $price->extra ?? '[]',
+            ])
+        );
+
+        $sync->successful = $prices->count() > 0;
+        $sync->finished_at = now();
+        $sync->prices_count = $prices->count();
+        $sync->save();
+
+        $this->info('Dispatching CheckPropertyPricesJob...');
+        dispatch(
+            new CheckPropertyPricesJob(
+                monitoredPropertyId: $propertyId,
+                propertyName: $propertyDTO->name,
+                platformSlug: $propertyDTO->platformSlug,
+            )
+        );
+
+        $this->table(['Check In', 'Price', 'Available', 'Extra'], $prices->map(fn ($price) => [
+            $price->checkin->toDateString(),
+            $price->price, $price->available,
+            json_encode($price->extra),
+        ])->toArray());
+
+        $this->line('Started at: ' . $startTimestamp->toDateTimeString());
+        $this->line('Finished at: ' . now()->toDateTimeString());
+        $this->line('Elapsed seconds: ' . $startTimestamp->diffInSeconds(now()));
+    }
+}
