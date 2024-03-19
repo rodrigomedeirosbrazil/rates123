ccfb4871796ba812958c2b20e3d5f61b7274271c
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-18 23:43:25 -0300

    chore: improve enum

--- app/Enums/DateEventTypeEnum.php
+++ app/Enums/DateEventTypeEnum.php
@@ -2,8 +2,12 @@
 
 namespace App\Enums;
 
+use App\Enums\Traits\Valuable;
+
 enum DateEventTypeEnum: string
 {
+    use Valuable;
+
     case Holiday = 'holiday';
     case Concert = 'concert';
     case Championship = 'championship';
