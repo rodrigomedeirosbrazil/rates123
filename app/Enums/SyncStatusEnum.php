d2cf9bd9076562817df37907f4e7c49c4fc30410
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-20 20:46:49 -0300

    chore: add valuable to enum

--- app/Enums/SyncStatusEnum.php
+++ app/Enums/SyncStatusEnum.php
@@ -2,8 +2,12 @@
 
 namespace App\Enums;
 
+use App\Enums\Traits\Valuable;
+
 enum SyncStatusEnum: string
 {
+    use Valuable;
+
     case Successful = 'successful';
     case Failed = 'failed';
     case InProgress = 'in_progress';
