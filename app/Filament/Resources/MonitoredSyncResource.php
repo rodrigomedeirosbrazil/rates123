27bec21e6dc7b3dcf2ba639e34f3b093022b11ef
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-20 20:46:58 -0300

    chore: adjust resource

--- app/Filament/Resources/MonitoredSyncResource.php
+++ app/Filament/Resources/MonitoredSyncResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\SyncStatusEnum;
 use App\Filament\Resources\MonitoredSyncResource\Pages;
 use App\Models\MonitoredSync;
-use Filament\Forms;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -23,20 +23,7 @@ class MonitoredSyncResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('monitored_property_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\Toggle::make('successful')
-                    ->required(),
-                Forms\Components\TextInput::make('prices_count')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\DateTimePicker::make('started_at')
-                    ->required(),
-                Forms\Components\DateTimePicker::make('finished_at')
-                    ->required(),
-            ]);
+            ->schema([]);
     }
 
     public static function table(Table $table): Table
@@ -46,14 +33,17 @@ class MonitoredSyncResource extends Resource
                 Tables\Columns\TextColumn::make('monitoredProperty.name')
                     ->searchable(isIndividual: true, isGlobal: false)
                     ->sortable(),
-                Tables\Columns\IconColumn::make('successful')
-                    ->boolean(),
+
+                Tables\Columns\TextColumn::make('status'),
+
                 Tables\Columns\TextColumn::make('prices_count')
                     ->numeric()
                     ->sortable(),
+
                 Tables\Columns\TextColumn::make('started_at')
                     ->dateTime()
                     ->sortable(),
+
                 Tables\Columns\TextColumn::make('finished_at')
                     ->dateTime()
                     ->sortable(),
@@ -61,9 +51,11 @@ class MonitoredSyncResource extends Resource
             ->searchOnBlur()
             ->filters([
                 Filter::make('Successful')
-                    ->query(fn (Builder $query): Builder => $query->whereSuccessful(true)),
-                Filter::make('Unsuccessful')
-                    ->query(fn (Builder $query): Builder => $query->whereSuccessful(false)),
+                    ->query(fn (Builder $query): Builder => $query->where('status', SyncStatusEnum::Successful->value)),
+                Filter::make('Failed')
+                    ->query(fn (Builder $query): Builder => $query->where('status', SyncStatusEnum::Failed->value)),
+                Filter::make('InProgress')
+                    ->query(fn (Builder $query): Builder => $query->where('status', SyncStatusEnum::InProgress->value)),
 
                 Filter::make('started_at')
                     ->form([
