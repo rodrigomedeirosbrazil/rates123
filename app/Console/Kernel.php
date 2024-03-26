2cc6526c69b975d3003b87b8393c5dc95a6635d2
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-25 23:24:43 -0300

    chore: schedule SendPriceNotificationEmailJob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\SendPriceNotificationEmailJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -10,6 +11,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:monitore')->dailyAt('12:00');
+        $schedule->job(SendPriceNotificationEmailJob::class)->dailyAt('15:00');
     }
 
     protected function commands(): void
