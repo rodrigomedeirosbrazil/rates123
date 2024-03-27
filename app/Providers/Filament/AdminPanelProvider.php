0b3f560a32df85f4bfd25e8d35f28214e3a0bb28
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-03-26 21:37:59 -0300

    chore: add plugin to admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use App\Filament\Resources\MonitoredSyncResource\Widgets\MonitoredSyncOverview;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -58,6 +59,7 @@ class AdminPanelProvider extends PanelProvider
             ->plugins([
                 FilamentApexChartsPlugin::make(),
                 FilamentFullCalendarPlugin::make(),
+                FilamentShieldPlugin::make(),
             ]);
     }
 }
