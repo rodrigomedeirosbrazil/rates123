da5a15d01a6322b0e01af745aaece2dfc1d9ff38
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-04-02 19:55:20 -0300

    chore: adjust labels and l10n

--- app/Filament/Shared/Resources/MonitoredPropertyResource.php
+++ app/Filament/Shared/Resources/MonitoredPropertyResource.php
@@ -13,8 +13,8 @@ use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -29,8 +29,8 @@ class MonitoredPropertyResource extends Resource
     {
         return $form
             ->schema([
-                Toggle::make('notify')
-                    ->label(__('Notify'))
+                Toggle::make('follow')
+                    ->label(__('Follow'))
                     ->afterStateHydrated(function ($component, $record) {
                         $component->state(
                             $record->users()->where('user_id', auth()->id())->exists()
@@ -47,10 +47,11 @@ class MonitoredPropertyResource extends Resource
 
                 Grid::make()->schema([
                     TextInput::make('name')
+                        ->label(__('Name'))
                         ->required(),
 
                     Select::make('monitored_platform_id')
-                        ->label('Platform')
+                        ->label(__('Platform'))
                         ->relationship(name: 'platform', titleAttribute: 'name')
                         ->preload() // ->searchable(['name'])
                         ->required(),
@@ -62,6 +63,7 @@ class MonitoredPropertyResource extends Resource
 
                 Grid::make()->schema([
                     Select::make('country')
+                        ->label(__('Country'))
                         ->options([
                             'Brasil' => 'Brasil',
                         ])
@@ -70,35 +72,41 @@ class MonitoredPropertyResource extends Resource
                         ->columnSpan(2),
 
                     TextInput::make('city')
+                        ->label(__('City'))
                         ->columnSpan(3),
 
                     Select::make('state')
+                        ->label(__('State'))
                         ->options(BrasilStatesEnum::toArray())
                         ->default('SP'),
 
                     TextInput::make('neighborhood')
+                        ->label(__('Neighborhood'))
                         ->columnSpan(2),
                 ])->columns(8),
 
                 Grid::make()->schema([
                     TextInput::make('address')
+                        ->label(__('Address'))
                         ->columnSpan(4),
 
-                    TextInput::make('number'),
+                    TextInput::make('number')
+                        ->label(__('Number')),
 
                     TextInput::make('complement')
+                        ->label(__('Complement'))
                         ->columnSpan(2),
 
-                    TextInput::make('postal_code'),
+                    TextInput::make('postal_code')
+                        ->label(__('Postal Code')),
 
                 ])->columns(8),
 
+                TextInput::make('latitude')
+                    ->label(__('Latitude')),
 
-
-
-                TextInput::make('latitude'),
-
-                TextInput::make('longitude'),
+                TextInput::make('longitude')
+                    ->label(__('Longitude')),
 
                 Textarea::make('extra')
                     ->columnSpanFull(),
@@ -110,33 +118,36 @@ class MonitoredPropertyResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('id')
+                    ->label('ID')
                     ->searchable(),
 
                 TextColumn::make('name')
+                    ->label(__('Name'))
                     ->searchable(),
 
                 TextColumn::make('platform.name')
+                    ->label(__('Platform'))
                     ->sortable(),
 
-                ToggleColumn::make('users')
-                    ->label(__('Notify'))
-                    ->disabled(),
-
                 TextColumn::make('created_at')
+                    ->label(__('Created At'))
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('updated_at')
+                    ->label(__('Updated At'))
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('deleted_at')
+                    ->label(__('Deleted At'))
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 Filter::make('Not Synced Today')
+                    ->label('Not Synced Today')
                     ->query(
                         fn (Builder $query): Builder => $query->whereDoesntHave(
                             'priceDatas',
@@ -145,16 +156,23 @@ class MonitoredPropertyResource extends Resource
                     ),
             ])
             ->actions([
+                Action::make('follow')
+                    ->icon(fn ($record) => $record->users()->where('user_id', auth()->id())->exists() ? 'heroicon-o-x-circle' : 'heroicon-o-user-plus')
+                    ->label(fn ($record) => $record->users()->where('user_id', auth()->id())->exists() ? __('Unfollow') : __('Follow'))
+                    ->color(fn ($record) => $record->users()->where('user_id', auth()->id())->exists() ? 'danger' : 'success')
+                    ->action(function ($record) {
+                        $exists = $record->users()->where('user_id', auth()->id())->exists();
+                        if ($exists) {
+                            $record->users()->detach(auth()->id());
+
+                            return;
+                        }
+
+                        $record->users()->attach(auth()->id());
+                    }),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\ViewAction::make(),
             ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
     }
 
     public static function getPages(): array
@@ -165,5 +183,10 @@ class MonitoredPropertyResource extends Resource
             'edit' => Pages\EditMonitoredProperty::route('/{record}/edit'),
             'view' => Pages\ViewMonitoredProperty::route('/{record}/view'),
         ];
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __('Property');
     }
 }
