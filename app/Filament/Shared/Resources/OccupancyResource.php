52fd6a9d38859160f00f83c7827c1470c39ca2d4
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-04-18 23:31:56 -0300

    chore: adjust resource

--- app/Filament/Shared/Resources/OccupancyResource.php
+++ app/Filament/Shared/Resources/OccupancyResource.php
@@ -3,15 +3,17 @@
 namespace App\Filament\Shared\Resources;
 
 use App\Filament\Shared\Resources\OccupancyResource\Pages;
-use App\Filament\Shared\Resources\OccupancyResource\RelationManagers;
 use App\Models\Occupancy;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
 
 class OccupancyResource extends Resource
 {
@@ -41,39 +43,78 @@ class OccupancyResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('monitored_property_id')
+                // TextColumn::make('monitoredProperty.name')
+                //     ->label(__('Property'))
+                //     ->sortable(),
+
+                TextColumn::make('checkin')
+                    ->label(__('Checkin'))
+                    ->formatStateUsing(fn (string $state): string => format_date_with_weekday($state))
+                    ->sortable(),
+
+                TextColumn::make('occupancyPercent')
+                    ->formatStateUsing(fn ($state): float => number_format($state, 2))
+                    ->label(__('Occupancy') . ' (%)'),
+
+                TextColumn::make('total_rooms')
                     ->numeric()
+                    ->label(__('Rooms'))
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->sortable(),
-                Tables\Columns\TextColumn::make('checkin')
-                    ->date()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('total_rooms')
+
+                TextColumn::make('occupied_rooms')
                     ->numeric()
+                    ->label(__('Occupied'))
+                    ->toggleable(isToggledHiddenByDefault: true)
                     ->sortable(),
-                Tables\Columns\TextColumn::make('occupied_rooms')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
+
+                TextColumn::make('created_at')
+                    ->label(__('Created At'))
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
+
+                TextColumn::make('updated_at')
                     ->dateTime()
+                    ->label(__('Updated At'))
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
+            ->searchOnBlur()
             ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+
+                // Filter::make('monitored_property_id')
+                //     ->form([
+                //         Select::make('monitored_property_id')
+                //             ->label(__('Property'))
+                //             ->searchable(['name'])
+                //             ->relationship(name: 'monitoredProperty', titleAttribute: 'name'),
+                //     ])
+                //     ->query(
+                //         fn (Builder $query, array $data): Builder => $query
+                //             ->when(
+                //                 $data['monitored_property_id'],
+                //                 fn (Builder $query, $value): Builder => $query->where('monitored_property_id', $value),
+                //             )
+                //     ),
+
+                Filter::make('checkin')
+                    ->label(__('Checkin'))
+                    ->form([
+                        DatePicker::make('checkin'),
+                    ])
+                    ->query(
+                        fn (Builder $query, array $data): Builder => $query
+                            ->when(
+                                $data['checkin'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('checkin', '=', $date),
+                            )
+                    ),
+
+            ], layout: FiltersLayout::Modal)
+            ->deferFilters()
+            ->defaultSort('created_at', 'desc')
+            ->paginated([10, 25, 50, 100]);
     }
 
     public static function getPages(): array
@@ -81,5 +122,15 @@ class OccupancyResource extends Resource
         return [
             'index' => Pages\ManageOccupancies::route('/'),
         ];
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __('Occupancy');
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __('Occupancies');
     }
 }
