d1d999f8d42788671c6a21e41e35fa7aa9705d6a
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-05-17 20:17:24 -0300

    chore: remove widgets from property resource

--- app/Filament/Shared/Resources/PropertyResource/Pages/ViewProperty.php
+++ app/Filament/Shared/Resources/PropertyResource/Pages/ViewProperty.php
@@ -3,8 +3,6 @@
 namespace App\Filament\Shared\Resources\PropertyResource\Pages;
 
 use App\Filament\Shared\Resources\PropertyResource;
-use App\Filament\Shared\Resources\PropertyResource\Widgets\PropertyPricesOverview;
-use App\Filament\Shared\Resources\PropertyResource\Widgets\PropertySyncOverview;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewProperty extends ViewRecord
@@ -13,9 +11,6 @@ class ViewProperty extends ViewRecord
 
     protected function getHeaderWidgets(): array
     {
-        return [
-            PropertyPricesOverview::class,
-            PropertySyncOverview::class,
-        ];
+        return [];
     }
 }
