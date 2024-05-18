eb292cae091287ba17dd6d9c783283266dc2db80
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-05-18 18:30:24 -0300

    chore: adjust migrations

--- database/migrations/2024_05_18_000001_create_property_followed_properties_table.php
+++ database/migrations/2024_05_18_000001_create_property_followed_properties_table.php
@@ -5,25 +5,20 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('user_property', function (Blueprint $table) {
+        Schema::create('property_followed_properties', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
             $table->foreignId('property_id')->constrained();
-            $table->string('role');
+            $table->foreignId('followed_property_id')->constrained();
             $table->timestamps();
         });
+
+        Schema::dropIfExists('user_followed_properties');
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('user_property');
+        Schema::dropIfExists('property_followed_properties');
     }
 };
