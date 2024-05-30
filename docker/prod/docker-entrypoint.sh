d056fd59689602d582c4a0e192020692cf1d3e09
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-05-29 23:29:03 -0300

    chore: add optimize to docker entrypoint

--- docker/prod/docker-entrypoint.sh
+++ docker/prod/docker-entrypoint.sh
@@ -3,5 +3,8 @@
 # echo "Changing Nginx default port to ${HTTP_NGINX_PORT}/${HTTPS_NGINX_PORT}"
 # /usr/bin/envsubst '$HTTP_NGINX_PORT,$HTTPS_NGINX_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+echo "Running optimize command..."
+/usr/local/bin/php /var/www/artisan optimize
+
 echo "Starting Supervisor..."
 /usr/bin/supervisord -c /etc/supervisord.conf
