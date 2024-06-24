5fcff9ccb4149230870b010c6401c5a91e47cab8
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-06-24 17:29:49 -0300

    chore: remove has api tokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,14 +9,12 @@ use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable implements FilamentUser
 {
-    use HasApiTokens;
     use HasFactory;
     use Notifiable;
     use HasRoles;
