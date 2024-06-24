b185416915a57dfd9a9ba533c997e34fd5c09a3a
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-06-24 17:28:21 -0300

    chore: update migration

--- database/migrations/2024_04_24_000001_migrate_tables.php
+++ database/migrations/2024_04_24_000001_migrate_tables.php
@@ -24,12 +24,6 @@ return new class () extends Migration {
             $table->unsignedBigInteger('property_id')->after('id')->nullable();
         });
 
-        DB::table('rates')->update(['property_id' => DB::raw('monitored_property_id')]);
-
-        Schema::table('rates', function (Blueprint $table) {
-            $table->dropColumn('monitored_property_id');
-        });
-
         Schema::table('properties', function (Blueprint $table) {
             $table->renameColumn('monitored_platform_id', 'scraped_platform_id');
         });
@@ -38,40 +32,16 @@ return new class () extends Migration {
             $table->unsignedBigInteger('property_id')->after('id')->nullable();
         });
 
-        DB::table('syncs')->update(['property_id' => DB::raw('monitored_property_id')]);
-
-        Schema::table('syncs', function (Blueprint $table) {
-            $table->dropColumn('monitored_property_id');
-        });
-
         Schema::table('user_followed_properties', function (Blueprint $table) {
             $table->unsignedBigInteger('property_id')->after('id')->nullable();
-        });
-
-        DB::table('user_followed_properties')->update(['property_id' => DB::raw('monitored_property_id')]);
-
-        Schema::table('user_followed_properties', function (Blueprint $table) {
-            $table->dropColumn('monitored_property_id');
         });
 
         Schema::table('price_notifications', function (Blueprint $table) {
             $table->unsignedBigInteger('property_id')->after('id')->nullable();
         });
 
-        DB::table('price_notifications')->update(['property_id' => DB::raw('monitored_property_id')]);
-
-        Schema::table('price_notifications', function (Blueprint $table) {
-            $table->dropColumn('monitored_property_id');
-        });
-
         Schema::table('occupancies', function (Blueprint $table) {
             $table->unsignedBigInteger('property_id')->after('id')->nullable();
-        });
-
-        DB::table('occupancies')->update(['property_id' => DB::raw('monitored_property_id')]);
-
-        Schema::table('occupancies', function (Blueprint $table) {
-            $table->dropColumn('monitored_property_id');
         });
 
         Schema::enableForeignKeyConstraints();
